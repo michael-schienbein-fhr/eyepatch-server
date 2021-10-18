82a664c3847b2a8289abf7f11d8ead9215183724
Michael Schienbein <moomba0@gmail.com>
2021-10-18 21:09:55 +0700

    comments / delete route

--- routes/rooms.js
+++ routes/rooms.js
@@ -5,28 +5,15 @@
 const jsonschema = require("jsonschema");
 
 const express = require("express");
-const { ensureCorrectUserOrAdmin, ensureAdmin, ensureRoomLoggedIn, ensureLoggedIn } = require("../middleware/auth");
+const { ensureCorrectUserOrAdmin, ensureAdmin, ensureLoggedIn } = require("../middleware/auth");
 const { BadRequestError } = require("../expressError");
 const Room = require('../models/room');
 const { createRoomToken } = require("../helpers/tokens");
 const roomNewSchema = require("../schemas/roomNew.json");
-// const roomUpdateSchema = require("../schemas/roomUpdate.json");
 
 const router = express.Router();
 
-
-/** POST / { room }  => { room, token }
- *
- * Adds a new room. This is not the registration endpoint --- instead, this is
- * only for admin users to add new rooms. 
- * 
- *
- * This returns the newly created room and an authentication token for them:
- *  {room: { id, roomOwner, roomName, hasPass }, token }
- *
- * Authorization required: admin
- **/
-
+// admin route for adding a room
 router.post("/", ensureAdmin, async function (req, res, next) {
   try {
     const validator = jsonschema.validate(req.body, roomNewSchema);
@@ -43,14 +30,7 @@ router.post("/", ensureAdmin, async function (req, res, next) {
   }
 });
 
-
-/** GET / => { users: [ {username, firstName, lastName, email }, ... ] }
- *
- * Returns list of all users.
- *
- * Authorization required: admin
- **/
-
+// get all rooms
 router.get("/", async function (req, res, next) {
   try {
     const rooms = await Room.findAll();
@@ -60,13 +40,7 @@ router.get("/", async function (req, res, next) {
   }
 });
 
-/** GET / => { users: [ {username, firstName, lastName, email }, ... ] }
- *
- * Returns list of all users.
- *
- * Authorization required: admin
- **/
-
+// gets the most recent room 
 router.get("/newest", async function (req, res, next) {
   try {
     const room = await Room.getNewest();
@@ -76,15 +50,7 @@ router.get("/newest", async function (req, res, next) {
   }
 });
 
-
-/** GET /[username] => { user }
- *
- * Returns { username, firstName, lastName, isAdmin, jobs }
- *   where jobs is { id, title, companyHandle, companyName, state }
- *
- * Authorization required: admin or same user-as-:username
- **/
-
+// gets room by id
 router.get("/:id", ensureLoggedIn, async function (req, res, next) {
   try {
     const room = await Room.get(req.params.id);
@@ -95,56 +61,8 @@ router.get("/:id", ensureLoggedIn, async function (req, res, next) {
   }
 });
 
-/** GET /[username] => { user }
- *
- * Returns { username, firstName, lastName, isAdmin, jobs }
- *   where jobs is { id, title, companyHandle, companyName, state }
- *
- * Authorization required: admin or same user-as-:username
- **/
-
-router.get("/:id/private", ensureRoomLoggedIn, async function (req, res, next) {
-  try {
-    const room = await Room.get(req.params.id);
-    return res.json({ room });
-  } catch (err) {
-    return next(err);
-  }
-});
-
-
-/** PATCH /[username] { user } => { user }
- *
- * Data can include:
- *   { firstName, lastName, password, email }
- *
- * Returns { username, firstName, lastName, email, isAdmin }
- *
- * Authorization required: admin or same-user-as-:username
- **/
-
-// router.patch("/:username", ensureCorrectUserOrAdmin, async function (req, res, next) {
-//   try {
-//     const validator = jsonschema.validate(req.body, roomUpdateSchema);
-//     if (!validator.valid) {
-//       const errs = validator.errors.map(e => e.stack);
-//       throw new BadRequestError(errs);
-//     }
-
-//     const user = await User.update(req.params.username, req.body);
-//     return res.json({ user });
-//   } catch (err) {
-//     return next(err);
-//   }
-// });
-
-
-/** DELETE /[username]  =>  { deleted: username }
- *
- * Authorization required: admin or same-user-as-:username
- **/
-
-router.delete("rooms/:id", ensureLoggedIn, async function (req, res, next) {
+// deletes room by id
+router.delete("/:id", ensureLoggedIn, async function (req, res, next) {
   try {
     await Room.remove(req.params.id);
     return res.json({ deleted: req.params.id });
